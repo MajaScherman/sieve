57ad81eadc0d0ce52407eb93c87e7a4f870cb1b1
Stazia Tronboll <stazia@tronboll.us>
2015-11-14 15:22:13 +0100

    Refactor for and Introduce the rVal object, which serves as our storage mechanism. Each value of R
    is granted an RVal object to reference at various stages of the algorithm.

--- src/src/Algo.java
+++ src/src/Algo.java
@@ -6,19 +6,87 @@ public class Algo {
 	// TODO this will have to change to something else
 	private int F[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 	private int L = 12; // TODO L=2^10
-	private BigInteger N;
+	private int fSize = F.length;
+        
+        private BigInteger N;
+        private RVal[] rValsArray;
 
 	public Algo(BigInteger N) {
 		this.N = N;
+                rValArray = new RVal[L];
+
+                // TODO get all r values, aka populate rValArray
+                createRList();
+
+                // TODO Get x*M = 0 Solutions
+
+                
+                // TODO cycle through above Solutions until gcd(y-x,N) = p or
+                // q. Send that p or q to testing
+
+
+                // TODO Test that solution is correct
 	}
 
-	private BigInteger rGenerator(BigInteger j, BigInteger k) {
-		return squareRoot((N.multiply(k))).add(j);// floor(sqrt(N*k))+j
-	}
+
+
+
+    /* HELPER METHODS */
+
+        private void createRList() {
+            int k = 0;
+            int j = 1;
+            int counter = 0; // run until we have L solutions
+            RVal testVal = null; // reference to the RVal that will be tested
+
+
+            while (counter < L) {
+                
+                // increment k and j in a staggered pattern, so that r grows
+                // more slowly. (e.g. (k,j) = (1,1);(1,2);(2,2);(2,3); etc..)
+                if (counter % 2 == 0) {
+                    k++;
+                } else {
+                    j++;
+                }
+
+                // get a new r to process
+                testVal = rGenerator(j,k);
+
+                // factor testVal
+                // if valid, store it and counter++; if invalid, throw away 
+                // TODO make parallel
+                
+
+
+
+            } // end master while
+        } // end createRList
+
+        /* Name: rGenerator
+         * Purpose: obtain a new value of r, create a matching RVal object, and
+         * set the rSquareMod field
+         * Parameters:
+         *      - BigInteger j,k : varying values which will yield new r's
+         * Return:
+         *      - Rval rVal : new value to test for validity in matrix
+         */
+        private RVal rGenerator(BigInteger j, BigInteger k) {
+            
+            // floor(sqrt(N*k))+j
+	    BigInteger r = squareRoot((N.multiply(k))).add(j);
+
+            RVal rVal = new RVal( r, matrixWidth);
+
+            // r = r^2 mod N
+            rVal.setRSquareMod(r.multiply(r).mod(N));
+
+            return rVal; 
+        }
 
 	/** Calculate the square root of a BigInteger in logarithmic time */
 	// naturally floors it?
-	public BigInteger squareRoot(BigInteger x) {
+	private BigInteger squareRoot(BigInteger x) {
 		BigInteger right = x, left = BigInteger.ZERO, mid;
 		while (right.subtract(left).compareTo(BigInteger.ONE) > 0) {
 			mid = (right.add(left)).shiftRight(1);
---
+++ src/src/RVal.java
@@ -0,0 +1,92 @@
+/*
+ * This object class holds all required data for a single r value. The fields
+ * are as follows:
+ *
+ * int[] binary : the binary row which will be written to a file and sent to
+ *                the gauss stage for solution generation. populated in
+ *                createRList
+ * int[] exponents : the unreduced row of exponents. Used in solution
+ *                   validation to form y in   x^2 = y^2 mod N. Populated in
+ *                   createRList. Note that each value is stored as half the
+ *                   true value, since we divide the exponents by 2 to achieve
+ *                   a Y^2 form.
+ * BigInteger r : the "namesake" of this object. Used in solution validation to
+ *                form x in x^2 = y^2 mod N. Populated upon instantiation.
+ * BigInteger rSquareMod : the value of r^2 mod N. Used immediately after
+ *                         rGeneration to populate binary and exponents.
+ *                         Populated in rGenerator.
+ * int largestFactorIdx : this is an optimization field. When the last factor is
+ *                        stored, its index in factorbase F is loaded into this
+ *                        field so that solution validation can stop at this
+ *                        greatest point instead of running through the full
+ *                        size of F.
+ *
+ * Class Algo holds a List of these objects as a global variable, for access in
+ * all stages. 
+ */
+
+
+public class RVal {
+    private int binary[];
+    private int exponents[];
+
+    private BigInteger r;
+    private BigInteger rSquareMod;
+
+    private int largestFactorIdx;
+
+    // Constructor
+    // @param BigInteger r : Y'know...the r
+    // @param int size : length of the factorbase, also length of binary and
+    //          exponents so that matching in solution validation is possible
+    public RVal(BigInteger r, int size) {
+        this.r = r;
+        binary = new int[size];
+        exponents = new int[size];
+    }
+
+    //
+    // Setters 
+    //
+
+    public void setBinaryValue( int idx, int val) {
+        binary[idx] = val;
+    }
+
+    public void setExponentsValue( int idx, int val) {
+        exponents[idx] = val;
+    }
+
+    public void setRSquareMod( BigInteger rSquareMod) {
+        this.rSquareMod = rSquareMod;
+    }
+
+    public void setLargestFactorIdx( int largestFactorIdx) {
+        this.largestFactorIdx = largestFactorIdx;
+    }
+
+    //
+    // Getters
+    //
+
+    public int[] getBinaryRow() {
+        return binary;
+    }
+
+    public int getExp(int idx) {
+        return exponents[idx];
+    }
+
+    public int getLargestFactorIdx() {
+        return largestFactorIdx;
+    }
+
+    public BigInteger getR() {
+        return r;
+    }
+
+    public BigInteget getRSquareMod() {
+        return rSquareMod;
+    }
+
+}
