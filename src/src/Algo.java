155d3b1d09289a63920a8516c2e8ebd286ed73c0
Stazia Tronboll <stazia@tronboll.us>
2015-11-15 22:30:59 +0100

    My last implementation for the night. A commit for commenting may follow.
    Functionality as it stands:
    We can generate a factorbase of whatever size we say. Part of Algo Class.
    An RArray object, which creates the testable Rs and delivers them to
        an RVal[] in Algo.
    SolutionsProcessor object, which retrieves the solutions from Gaussian elimination
    and searches for a factor. This implentation needs further testing after
        Gauss is implemented.
    I made a Lamo pass at check the factors yielded by SolutionsProcessor, but
        haven't tested in the slightest.
    
    Still need to...
        1) Implement the Gaussian elimination
        2) Find a way to make all this stuff parallel - it'll help efficiency
        3) Really check the factors, and possibly deliver them back to Main
        4) Vary j and k better - don't know how, but there has to be a better way

--- src/src/Algo.java
+++ src/src/Algo.java
@@ -1,13 +1,16 @@
 package src;
 
-import java.math.BigInteger;
+import java.math.*;
+import java.util.Arrays;
 
 public class Algo {
 	// TODO this will have to change to something else
 	private int certainty = 1000;
 	private int F[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
-	private int L = 12; // TODO L=2^10
-	private int fSize = F.length;
+	private int fSize;
+	private int L; // TODO L=2^10
+	private final int BUFFER_CONST = 24;
+
         
     private BigInteger N;
     private RVal[] rValArray;
@@ -16,24 +19,27 @@ public class Algo {
 		this.N = N;
 
         // generate factorbase
-        
+        generateFactorbase(15);
+        fSize = F.length;
+        L = fSize + BUFFER_CONST;
         
         // get all r values, aka populate rValArray
 		RArray rArray = new RArray(L,fSize,N, F);
 		rValArray = rArray.getArray();
-
-        // TODO Get x*M = 0 Solutions
-		SolutionsManager sm = new SolutionsManager();
-        
-        // TODO cycle through above Solutions until gcd(y-x,N) = p or
-        // q. Send that p or q to testing
-		// Tentatively finished - must edit to fit Gaussian implementation
-        SolutionsProcessor solnProcessor = new SolutionsProcessor(rValArray, F, sm, N);
-        BigInteger q = solnProcessor.findFactor();
-
-        // TODO Test that solution is correct
-        BigInteger p = N.divide(q);
-        checkQ(q, p);
+		
+//        // TODO Get x*M = 0 Solutions
+//		SolutionsManager sm = new SolutionsManager();
+//        
+//        // TODO cycle through above Solutions until gcd(y-x,N) = p or
+//        // q. Send that p or q to testing
+//		// Tentatively finished - must edit to fit Gaussian implementation
+//        SolutionsProcessor solnProcessor = 
+//        		new SolutionsProcessor(rValArray, F, sm, N);
+//        BigInteger q = solnProcessor.findFactor();
+//
+//        // TODO Test that solution is correct
+//        BigInteger p = N.divide(q);
+//        checkQ(q, p);
 	}
 
     /* HELPER METHODS */
@@ -46,5 +52,28 @@ public class Algo {
 				p.isProbablePrime(certainty));
 		
 		System.out.println(N.toString() + " == " + q.multiply(p).toString() + " ?");
+	}
+	
+	void generateFactorbase(int size) { 
+		int n = 0;
+		int i, j, k;
+		F = new int[size]; // degree of 24
+		
+		for(i = 2; n < size; ++i) { 
+			k = (int) (Math.sqrt((double) i) + 1);
+		    
+			for(j=0; j < n && F[j] <= k; ++j) {
+				if( (i % F[j]) == 0) {
+					j = 0; 
+					break; 
+				}
+			}
+		
+		    if (j != 0 || n == 0) { 
+		    	F[n++]= i;
+		    	System.out.print(F[n-1] + ", ");
+			}
+		}
+		System.out.println();
 	}
 }
---
+++ src/src/TestAlgo.java
@@ -0,0 +1,16 @@
+package src;
+
+import static org.junit.Assert.*;
+import java.math.BigInteger;
+
+import org.junit.Test;
+
+public class TestAlgo {
+	
+	@Test
+	public void testGenerateFactorbase() {
+		new Algo(new BigInteger("16637"));
+		return;
+	}
+
+}
