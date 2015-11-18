b313e1761720f70907b7b1aa8c662e57ac9877a2
Maja <maja.scherman@gmail.com>
2015-11-18 19:42:57 +0100

    Started refactorising and moved main into algo

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,11 +1,9 @@
 package src;
 
+import java.math.BigInteger;
+
 public class Main {
 
-	   public static void main(String[] args) {
-	        System.out.println("Hello, World");
-	        
-	    }
-	   
-	   
+	
+
 }
